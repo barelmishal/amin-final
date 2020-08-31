9b23a8c6d34f9b0c88ae5ccdb669a1601af4c5e7
Barel Mishal <dreamwork@dreamwork.network>
2020-08-31 20:08:10 +0300

    delete commit

--- src/pages/food-search/food-search.jsx
+++ src/pages/food-search/food-search.jsx
@@ -10,6 +10,7 @@ import hamburgerIcon from '../../pic/hamburger-icon.svg';
 const FoodSearch = () => (
     <div className="food-search">
             <section className="user-nav"> 
+            {/* אני מנסה להבין כיצד עושים אקרדיון */}
                   {/* <button className="back-main-page" id='back-btn-main-page'>BACK TO MAIN PAGE</button> */}
                   <div className="title" id='title'>welcome Ron Levi</div>
                   {/* <button className="logout" id='log-out'>log out</button> */}
@@ -18,9 +19,23 @@ const FoodSearch = () => (
             <section className='bar-steps'>
                 <div className="bar-steps-title" id="bar-steps">step 1: search and chose foods items</div>
             </section>
+
             <section className="food-search-bar">
-                <div className="container-food-search">
-                    <input type="search" name="search-food-items" id="search-food-items" placeholder="search foods items"/>
+                <div className="input-container">
+                    <input 
+                    type="search" 
+                    name="items" 
+                    id="items" 
+                    placeholder="search foods items"/>
+                </div>
+                <div className="result-list">
+                    <div className="result">
+                        <div className="category">
+                            <div className="description">
+                                
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </section>
     </div>
