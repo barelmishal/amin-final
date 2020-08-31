f82c374d0e1d2a746697b279079d55f3de30e729
Barel Mishal <dreamwork@dreamwork.network>
2020-08-31 16:50:39 +0300

    search design and try to do menu that come down

--- src/pages/food-search/food-search.jsx
+++ src/pages/food-search/food-search.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import './food-search.css';
+//photo
+import hamburgerIcon from '../../pic/hamburger-icon.svg';
 
 
 
 
 
 const FoodSearch = () => (
-    <div className="main-foodsearch">
-        
+    <div className="food-search">
+            <section className="user-nav"> 
+                  {/* <button className="back-main-page" id='back-btn-main-page'>BACK TO MAIN PAGE</button> */}
+                  <div className="title" id='title'>welcome Ron Levi</div>
+                  {/* <button className="logout" id='log-out'>log out</button> */}
+                  <img className="menu-icon" src={hamburgerIcon} />
+            </section>
+            <section className='bar-steps'>
+                <div className="bar-steps-title" id="bar-steps">step 1: search and chose foods items</div>
+            </section>
+            <section className="food-search-bar">
+                <div className="container-food-search">
+                    <input type="search" name="search-food-items" id="search-food-items" placeholder="search foods items"/>
+                </div>
+            </section>
     </div>
 );
 
