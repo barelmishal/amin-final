56d2215d303502c8a3535bf853a1c93ca1dbdbe2
Barel Mishal <dreamwork@dreamwork.network>
2020-09-02 19:21:08 +0300

    search bar open the query

--- src/pages/food-search/food-search.jsx
+++ src/pages/food-search/food-search.jsx
@@ -3,64 +3,35 @@ import './food-search.css';
 // photo
 import hamburgerIcon from '../../pic/hamburger-icon.svg';
 
-
 const FDC_API_KEY = process.env.REACT_APP_FDC_API_KEY;
-
-
 
 class FoodSearch extends Component {
     constructor() {
-        super();
+        super(); 
         this.state = {
-            query: "",
-            results: null,
-            selection: ""
+            query: ''
+        }
+    }
+    
+    // componentDidMount(event) {
+    //    
+    //     this.setState({query});
+    //     this.timeout = setTimeout = 
+    //     fetch(`https://api.nal.usda.gov/fdc/v1/foods/search?query=${encodeURIComponent(query)}&pageSize=10&api_key=${FDC_API_KEY}`)
+    //     .then(response => console.log(response));
+    // }
+    onSearch = (event) => {
+        const query = event.target.value;
+        this.setState({query});
+        if (!query) {
+            this.setState({results: null});
+        } else {
+
         }
     }
 
-    onTextChange(event) {
-        const query = event.target.value;
-        this.setState({query});
-    
-        if (!query) {
-          this.setState({results: null});
-        } else {
-          if (this.timeout !== null) {
-            clearTimeout(this.timeout);
-          }
-          this.timeout = setTimeout(() => {
-            fetch(`https://api.nal.usda.gov/fdc/v1/foods/search?query=${encodeURIComponent(query)}&pageSize=10&api_key=${FDC_API_KEY}`)
-            .then(resp => resp.json())
-            .then(results => {
-              const fdcIds = results.foods.map(food => food.fdcId).join(',');
-        
-              return fetch(`https://api.nal.usda.gov/fdc/v1/foods/?fdcIds=${fdcIds}&api_key=${FDC_API_KEY}`);
-            })
-            .then(resp => resp.json())
-            .then(results => {
-              const categoryMap = new Map();
-              if (Array.isArray(results)) {
-                for (const food of results) {
-                  const category = food.brandedFoodCategory || 'Other';
-                  const foods = categoryMap.get(category);
-                  if (!foods) {
-                    foods = new Map();
-                    categoryMap.set(category, foods);
-                  }
-                  foods.set(food.description, food);
-                }
-    
-                this.setState({results: categoryMap});
-              } else {
-                this.setState({results: new Map()});
-              }
-            })
-          }, 500);
-        }
-      }
-
     render() {
-        return (  
+        return (
             <div className="food-search">
                     <section className="user-nav"> 
                     {/* אני מנסה להבין כיצד עושים אקורדיון */}
@@ -74,48 +45,26 @@ class FoodSearch extends Component {
                     </section>
         
                     <section className="food-search-bar">
-                        <div className="input-container">
+                        {/* ברגע שכותבים אז הID משתנה ויש לנו ID there-is-result */}
+                        <div className="input-container" id={this.state.query}>
                             <input 
                             type="text" 
-                            value={this.state.query}
-                            onChange={this.onTextChange}
+                            // value={this.state.query} 
+                            onChange={this.onSearch}
                             name="items" 
                             id="items" 
                             placeholder="search foods items"/>
                         </div>
-                        {this.state.results && (
-                            <div className="result-list">
-                            {Array.from(this.state.results.entries()).map(entry => {
-                                const category = entry[0]; 
-                                const foods = entry[1]; 
-                                return (
-                                <div key={category} className="result">
-                                    {/* Display the category in gray */}
-                                    <div className="category">{category}</div>
-                                    {/* Display the list of foods in the category in black under the category */}
-                                    {Array.from(foods.values()).map(food => (
-                                    <div onClick={() => this.select(food)} key={food.fdcId} className="description">{food.description}</div>
-                                    ))}
-                                </div>
-                                )
-                            })}
-                            {/* Show a _no results_ message if the search didn't return any results */}
-                            {!this.state.results.size && (<div>לא נמצאו תוצאות</div>)}
-                            </div>
-                        )}   
-                           
-                           
-                           
-                            {/* {
-                            <div className="result-list">
-                                <div className="result">
-                                    <div className="category">
-                                        <div className="description">
+                            {this.state.query && (
+                                <div className="result-list">
+                                    <div className="result">
+                                        <div className="category">
+                                            <div className="description">
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                            } */}
+                            )}  
                     </section>
             </div>
         )
