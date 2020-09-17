16446a7ace395bee4214819033e3287989069ad4
Barel Mishal <dreamwork@dreamwork.network>
2020-09-17 15:06:40 +0300

    back to original

--- server/routes/foodslist.js
+++ server/routes/foodslist.js
@@ -18,73 +18,70 @@ router.post("/chose", async (req, res, next) => {
     } else {
       trx = await db.transaction();
       const FoodData = await axios.get(`https://api.nal.usda.gov/fdc/v1/foods/?fdcIds=${fdcFoodId}&api_key=${FDC_API_KEY}`).then(resp => resp.data);
-      
-
+      let categoryIs = '';
 
       console.log(FoodData); 
       // boolen value that needed to get Category MeasureUnit
-      let branded    = (FoodData[0].dataType === 'Branded');
-      let srLegacy   = (FoodData[0].dataType === 'SR Legacy');
-      let foundation = (FoodData[0].dataType === 'Foundation');
-      
-      let dataOrgi;
-      const getDataOrgi = () => {
-        dataOrgi = [
-        {
-          // add to foods dataOrgi[0]
-          fdc_id: fdcFoodId,
-          food_description: FoodData[0].description,
-        },
-        {
-          // adds to measure_units dataOrgi[1]
-          Branded: null,
-          SrLegacy: null,
-          Foundation: null,
-          survay: null
-        },
-        {
+      let Branded    = (FoodData[0].dataType === 'Branded');
+      let SrLegacy   = (FoodData[0].dataType === 'SR Legacy');
+      let Foundation = (FoodData[0].dataType === 'Foundation');
 
+      // import foods
+      const getCategory = () => {
+        if (Branded) {
+          categoryIs = FoodData[0].brandedFoodCategory;
+        } else if (SrLegacy || Foundation) {
+          categoryIs = FoodData[0].foodCategory.description;
+        } else {
+          categoryIs = FoodData[0].wweiaFoodCategory.wweiaFoodCategoryDescription;
         }
-        ]
-        if (branded) {
-          dataOrgi[0].food_category = FoodData[0].brandedFoodCategory;
-          dataOrgi[1].Branded.measure_unit_name = FoodData[0].householdServingFullText;
-          dataOrgi[1].Branded.abbreviation = null;
-          dataOrgi[1].Branded.fdc_id = fdcFoodId;
-        } else if (srLegacy) {
-          dataOrgi[0].food_category = FoodData[0].foodCategory.description;
-          dataOrgi[1].SrLegacy = FoodData[0].foodPortions.map(p => ({
-            measure_unit_name: p.modifier,
+        return categoryIs
+      }
+      getCategory()
+      const [food, category, fdcId] = [FoodData[0].description, categoryIs, fdcFoodId];
+      const insertNewFoodToDb = await trx('foods').insert([
+      {
+        food_category: category,
+        fdc_id: fdcId,
+        food_description: food
+      },
+      ], 'id');
+
+      // import measure unit
+      let dbmeasureUnit;
+      const getMeasureUnit = () => {
+        if (Branded) {
+            dbmeasureUnit = {
+            measure_unit_name: FoodData[0].householdServingFullText,
             abbreviation: null,
             fdc_id: fdcId
-          }));
-        } else if (foundation) {
-          dataOrgi[0].food_category = FoodData[0].foodCategory.description;
-          dataOrgi[1].Foundation = FoodData[0].foodPortions.map(p => ({
+          }
+        } else if (Foundation) {
+            dbmeasureUnit = FoodData[0].foodPortions.map(p => ({
             measure_unit_name: p.measureUnit.name,
             abbreviation: p.measureUnit.abbreviation,
             fdc_id: fdcId
           }));
-        } else {
-          dataOrgi[0].food_category = FoodData[0].wweiaFoodCategory.wweiaFoodCategoryDescription;
-          dataOrgi[1].survay = FoodData[0].foodPortions.map(p => ({
-            measure_unit_name: p.portionDescription,
+        } else if (SrLegacy) {
+            dbmeasureUnit = FoodData[0].foodPortions.map(p => ({
+              measure_unit_name: p.modifier,
             abbreviation: null,
             fdc_id: fdcId
           }));
+        } else {
+            dbmeasureUnit = FoodData[0].foodPortions.map(p => ({
+              measure_unit_name: p.portionDescription,
+              abbreviation: null,
+              fdc_id: fdcId
+            }));
+          }
+          return dbmeasureUnit
         }
-      }
-      getDataOrgi()
-      console.log(dataOrgi)
-       
-      const insertNewFoodToDb = await trx('foods').insert(dataOrgi[0], 'id');
+      getMeasureUnit();
       const insertNewMeasurUnits = await trx('measure_units').insert(dbmeasureUnit, 'id');
-      // after got data for two of this food we need to join them and add amounts
-      const insertNewFoodPortions = await trx('food_portions').insert(dbmeasureUnit, 'id');
-      // after the insertion of "food_portions" we get nutrients_id and filter from what we have in database
-      // after that join the table with food nutrients
+      console.log(insertNewMeasurUnits, dbmeasureUnit);
 
-
+      // import lj
       await trx.commit();
 
 
