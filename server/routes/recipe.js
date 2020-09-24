68aa3ca9b8c0e3c8184de1dc671909059e07addd
Barel Mishal <dreamwork@dreamwork.network>
2020-09-24 19:50:54 +0300

    respond to pull requst commnets

--- server/routes/recipe.js
+++ server/routes/recipe.js
@@ -1,24 +1,20 @@
-const axios = require('axios').default;
 var express = require('express');
-var jwt = require('jsonwebtoken');
-const { whereIn } = require('../db');
 var router = express.Router();
 var db = require('../db');
 
-const FDC_API_KEY = process.env.REACT_APP_FDC_API_KEY;
 
 router.post("/", async (req, res, next) => {
   let trx;
   try {
-      trx = await db.transaction();
-      const recipe_name = await trx('recipes').insert({recipe_description: 'recipe 1'}, 'id');
+    trx = await db.transaction();
+    const recipeIds = await trx('recipes').insert({recipe_description: 'Untitled  recipe'}, 'id');
     await trx.commit();
     
     res.json({
-        IdRecipeName: recipe_name[0]
+      IdRecipeName: recipeIds[0]
     })
   } catch (err) {
-    console.error(err);
+      console.error(err);
     if (trx) {
       await trx.rollback(err)
     }
