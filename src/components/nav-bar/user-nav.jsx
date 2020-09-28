89c0da35e41eade357a277883d442ba7bf18daab
Barel Mishal <dreamwork@dreamwork.network>
2020-09-28 17:07:30 +0300

    welcome page staling finish

---
+++ src/components/nav-bar/user-nav.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Action from "../buttons/back-to/action";
+import "./user-nav.css";
+
+const UserNav = (props) => {
+  return (
+    <div className="user-nav">
+      <Action btnTatile="main page" className="main-page" />
+      <div className="title" id="title">
+        welcome Ron Levi
+      </div>
+      <Action btnTatile="sign out" className="sign-out" />
+    </div>
+  );
+};
+
+export default UserNav;
