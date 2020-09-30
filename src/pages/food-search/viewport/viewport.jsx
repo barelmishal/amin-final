115a643a2d79a2ed11789395c42aaef0880e70a9
Barel Mishal <dreamwork@dreamwork.network>
2020-09-30 18:13:15 +0300

    comments delets

--- src/pages/food-search/viewport/viewport.jsx
+++ src/pages/food-search/viewport/viewport.jsx
@@ -4,51 +4,11 @@ import "./viewport.css";
 class Viewport extends React.Component {
   constructor() {
     super();
-    this.state = {
-      width: 800,
-      height: 182,
-    };
-  }
-
-  /**
-   * Calculate & Update state of new dimensions
-   */
-  updateDimensions() {
-    if (window.innerWidth < 500) {
-      this.setState({ width: 450, height: 102 });
-    } else {
-      let update_width = window.innerWidth - 100;
-      let update_height = Math.round(update_width / 4.4);
-      this.setState({ width: update_width, height: update_height });
-    }
-  }
-
-  /**
-   * Add event listener
-   */
-  componentDidMount() {
-    this.updateDimensions();
-    window.addEventListener("resize", this.updateDimensions.bind(this));
-    console.log();
-  }
-
-  /**
-   * Remove event listener
-   */
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.updateDimensions.bind(this));
+    this.state = {};
   }
 
   render() {
-    return (
-      <div
-        width={this.state.width}
-        height={this.state.height}
-        className="viewport"
-      >
-        {this.props.children}
-      </div>
-    );
+    return <div className="viewport">{this.props.children}</div>;
   }
 }
 
