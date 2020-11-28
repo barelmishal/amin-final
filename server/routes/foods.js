20788e46d4cd51c3704fb8a3be0eb57e6c6038ba
Barel Mishal <dreamwork@dreamwork.network>
2020-11-28 19:50:55 +0200

    measuer unit and measuer unit names adding to server route

--- server/routes/foods.js
+++ server/routes/foods.js
@@ -4,6 +4,20 @@ const csv = require("csv-parser");
 const fs = require("fs");
 const path = require("path");
 
+const measureUnits = [];
+fs.createReadStream(path.join(__dirname, "../csv_db/midot.csv"))
+  .pipe(csv())
+  .on("data", (data) => {
+    measureUnits.push(data);
+  });
+
+const foodPortions = [];
+fs.createReadStream(path.join(__dirname, "../csv_db/midot_mitzrachim.csv"))
+  .pipe(csv())
+  .on("data", (data) => {
+    foodPortions.push(data);
+  });
+
 router.get("/search", async (req, res, next) => {
   const query = req.query.q;
   const results = [];
@@ -15,7 +29,18 @@ router.get("/search", async (req, res, next) => {
       }
     })
     .on("end", () => {
-      res.json(results);
+      res.json(
+        results.map((r) => ({
+          ...r,
+          foodPortions: foodPortions
+            .filter((p) => p.mmitzrach === r.id)
+            .map((m) => ({
+              ...m,
+              measureUnitName: measureUnits.find((u) => u.smlmida === m.mida)
+                .shmmida,
+            })),
+        }))
+      );
     });
 });
 
